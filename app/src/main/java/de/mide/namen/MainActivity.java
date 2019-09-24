7215937ed8386d902afce47105724b0f6c026827
Daniel Lupu <dlupu@united.domain>
2019-09-24 13:06:00 +0200

    Initial commit

---
+++ app/src/main/java/de/mide/namen/MainActivity.java
@@ -0,0 +1,23 @@
+package de.mide.namen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+    protected String Namen;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        _nameNamen = findViewById(R.id.editText);
+        Button button = (Button)
+    }
+
+    public void onClick(View v){
+
+    }
+}
